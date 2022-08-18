5c64557865c7e3843b65ae8af86e9608ceddc29e
Victor-HM <victor.27cm.santos@gmail.com>
2022-08-17 22:46:37 -0300

    modelo pronto, hover funcionando

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,7 +1,48 @@
+import { Circle, Student } from 'phosphor-react'
+import './style.css'
 
+function Subjects({ name, description1, description2 }) {
+    return (
+        <div className='subjects'>
+            <div className='headerSubject'>
+                <Circle size={20} color='#FF914D' weight='fill' />
+                <p>{name}</p>
+            </div>
+
+            <p>{description1}</p>
+            <p>{description2}</p>
+            <button className='logoff'>Trocar de curso</button>
+        </div>
+    )
+}
 
 export function User() {
     return (
-        <div>asd</div>
+        <div className='containerUser'>
+            <div className='contentUser'>
+                <div className='infoUser'>
+                    <h1 className='titleUser'>
+                        Perfil do <span>Aluno</span>
+                    </h1>
+
+                    <div className='photoUser'>
+                        <div className='photo'>
+                            <Student size={90} />
+                        </div>
+
+                        <div className='nameUser'>
+                            <p>Victor Hugo Carvalho Moreira dos Santos</p>
+                            <p>ETEC Cidade Tiradentes</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='detailsUser'>
+
+                </div>
+            </div>
+
+            <Subjects name='Desenvolvimento de Sistemas' description1='Complemento curricular de genero técnico, para abtenção da habilitação técnica.' description2='Complemento curricular de genero técnico, para abtenção da habilitação técnica.' />
+        </div>
     )
 }
--- src/routes/RouteNavigation.js
+++ src/routes/RouteNavigation.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 
 import { Header } from "../components/header/Header";
 import { Main } from "../components/mainScreen/main/Main";
@@ -9,16 +9,15 @@ import { Navbar } from "../components/navbar/Navbar";
 import "./style.css";
 import { useContext } from "react";
 import { AuthContext } from "../provider/Auth";
-import { RegisterSchool } from "../components/registerSchool";
 
 export function RouteNavigation() {
   const { user, setUser } = useContext(AuthContext)
+  let location = useLocation()
 
   return (
     <>
-      { user.auth ? <Header /> : <div></div> }
+      { location.pathname != '/' ? <> <Header /> <Navbar /> </> : <div></div> }
     <div className="navbarContent">
-      { user.auth ? <Navbar /> : <div></div> }
 
       <div className="components">
         <Routes>
@@ -26,7 +25,6 @@ export function RouteNavigation() {
           <Route exact path="/main" element={<Main />} />
           <Route path="/form" element={<Form />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/register-school" element={<RegisterSchool />} />
           <Route path="/boletim" element={<Boletim />} />
         </Routes>
       </div>
