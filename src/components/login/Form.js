d223879ab0c70122a8bd6c123d9438512977e005
Alison Christian <alison.carvalho@etec.sp.gov.br>
2022-08-19 05:42:02 -0300

    Multi Step form on Login

--- src/components/login/Form.js
+++ src/components/login/Form.js
@@ -4,11 +4,11 @@ import { useState } from 'react'
 import { useContext } from 'react';
 import { AuthContext } from '../../provider/Auth';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 
 
 export function Form() {
-    const [text, setText] = useState('');
+    const [text, setText] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [code, setCode] = useState('')
@@ -16,6 +16,27 @@ export function Form() {
     const navigate = useNavigate()
 
     const { user, setUser } = useContext(AuthContext)
+
+    const [currentStep, setCurrentStep] = useState(0)
+    
+    const steps = [
+        {
+            id: "USER",
+            title: "Selecionar usuário"
+        },
+        {
+            id: "LOGIN",
+            title: "Inserir login e senha"
+        }
+    ];
+
+    function handleNextStep () {
+        setCurrentStep((prevState) => prevState +1)
+    }
+
+    function handlePrevStep () {
+        setCurrentStep((prevState) => prevState -1)
+    }
 
     function handleSubmit() {
         setUser({ auth: true })
@@ -40,46 +61,43 @@ export function Form() {
                         
                         <h2 className='title'> Acesse a sua <span> conta</span></h2>
                     </div>
-
-                   
-
-
-                    
+            
                     <div className="screen">
 
-                         {/*
+                         {steps[currentStep].id === 'USER' && (
                         
                         
-                        <div className="contentMid">
-                            <select name="usuario">
-                                <option>Aluno</option>
-                                <option>Professor</option>
-                                <option>Coordenador</option>
-                            </select>
-                            <input 
-                                type="text"
-                                placeholder="Código" 
-                                name="code" 
-                            />
-                        </div>
-                        <input 
-                        type='email'
-                        name="email" 
-                        placeholder="Insira o Email"
-                        />
-                        <input 
-                        type='password'
-                        name="password" 
-                        placeholder="Insira a senha"  
-    
+                        <><><div className="contentMid">
+                                <select className="selectAluno" name="usuario">
+                                    <option>Aluno</option>
+                                    <option>Professor</option>
+                                    <option>Coordenador</option>
+                                </select>
+                                
+                            </div></></>
 
-                       
-                        
-                        <button type='submit' className="button" onClick={handleSubmit} >Entrar</button>
+                                )} {steps[currentStep].id === 'LOGIN' && (
 
-                        /> */}
+                                <><input
+                                type='email'
+                                name="email"
+                                placeholder="Insira o Email" /><input
+                                    type='password'
+                                    name="password"
+                                    placeholder="Insira a senha" />
+                                    
+                                    </>
+                                    )}
 
-                        <FontAwesomeIcon size="xl" color='white' icon={faAngleRight} />
+{currentStep < steps.length - 1 && (
+                                      <FontAwesomeIcon onClick={handleNextStep} size="xl" color='white' icon={faAngleRight} />
+
+)}
+
+{currentStep === steps.length - 1 && (
+                                          <><FontAwesomeIcon className="backButton" onClick={handlePrevStep} size="x" color='white' icon={faAngleLeft} /><button type='submit' className="button" onClick={handleSubmit}>Entrar</button></>
+
+)}
 
 
                     </div>
