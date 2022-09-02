c3280e8b0cf6df21ba5a37dcae43a12aef514313
Alison Christian <alison.carvalho@etec.sp.gov.br>
2022-09-02 13:47:21 -0300

    register.js multistep form + refactoring (incomplete)

--- client/src/pages/register/student/Register.js
+++ client/src/pages/register/student/Register.js
@@ -1,5 +1,11 @@
 import {useContext, useState} from 'react'
 import LoadingButton from '@mui/lab/LoadingButton';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import IconButton from '@mui/material/IconButton';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 import './register.css'
 
@@ -31,25 +37,107 @@ export function RegisterStudent() {
     password: "",
   });
 
+  const [currentStep, setCurrentStep] = useState(0)
+
+    const steps = [
+        {
+            id: "USERNAME",
+            title: "Primeiro, insira seu ",
+            span: "nome",
+            
+        },
+        {
+            id: "SCHOOLID",
+            title: "Insira o código da sua escola",
+            span: "código"
+        },
+        {
+            id: "USER",
+            title: "Insira o seu e-mail e escolha uma senha",
+            span: "e-mail"
+        },
+        {
+            id: "CONFIRMPASSWORD",
+            title: "Por favor, confirme sua ",
+            span: "senha"
+        }
+    ];
+
+    const CustomBackButton = {
+      position: "absolute", 
+      top: "14px", 
+      left: "5px",
+      transition: "all 0.5s ease-out",
+      "&:hover": {
+        transform: "rotateZ(360deg)"
+      },
+    }
+
+    function handleNextStep () {
+        setCurrentStep((prevState) => prevState +1)
+    }
+
+    function handlePrevStep () {
+        setCurrentStep((prevState) => prevState -1)
+    }
+
+
   return (
+
+    
+  <div className="flex">
+
+    {/* Animação de estrelas do fundo da tela */}
+    <div className="stars">
+        <div className="starsec"></div>
+        <div className="starthird"></div>
+        <div className="starfourth"></div>
+        <div className="starfifth"></div>
+      </div>
+    
       <div className='auth'>
-                <h2>Faça o seu <span>cadastro</span></h2>
 
-        <form name='registration_form'>
-          <input
-            type='text'
-            placeholder="Digite seu nome e sobrenome"
-            name='name'
-            required
-          />
 
-          <input 
-            type='text' 
-            name='code'
-            placeholder="Digite o código de sua escola"
-            required
-          />
+{currentStep > 1 && (
+    <IconButton sx={CustomBackButton} onClick={handlePrevStep} fontaria-label="back to select page">
+    <ChevronLeftIcon sx={{color: "white"}} fontSize="large"/>
+  </IconButton> 
 
+)}
+
+       
+
+      <form name='registration_form'>
+
+      {steps[currentStep].id === 'USERNAME' && (
+        <>
+                <h2>{steps[currentStep].title} <span>{steps[currentStep].span}</span></h2>
+
+                <input
+                  type='text'
+                  placeholder="Digite seu nome e sobrenome"
+                  name='name'
+                  required
+                />
+        </>
+      )}
+      {steps[currentStep].id === 'SCHOOLID' && (
+        <>
+                <h2>{steps[currentStep].title} <span>{steps[currentStep].span}</span></h2>
+
+                <input 
+                  type='text' 
+                  name='code'
+                  placeholder="Digite o código de sua escola"
+                  required
+                />
+        </>
+      )}
+      {steps[currentStep].id === 'SCHOOLID' && (
+        <>
+                <h2>{steps[currentStep].title} <span>{steps[currentStep].span}</span></h2>
+
+                
           <input 
             type='email' 
             name='email'
@@ -63,13 +151,28 @@ export function RegisterStudent() {
             required
             placeholder='Digite sua senha'
             />
+        </>
+      )}
+      {steps[currentStep].id === 'SCHOOLID' && (
+        <>
+                <h2>{steps[currentStep].title} <span>{steps[currentStep].span}</span></h2>
 
-            <input 
+                
+                <input 
             type='password'
             name='confirmPassword'
             required
             placeholder='Confirme sua senha'
             />
+        </>
+      )}
+
+
+
+
+
+<Button sx={{borderRadius: "1000px", width: "0"}} variant="outlined" onClick={handleNextStep} startIcon={<ChevronRightIcon />} />
+
 
       <LoadingButton loading={isInputChanged ? true : false} sx={LoadingStyles} variant="outlined">
        Cadastrar
@@ -77,6 +180,7 @@ export function RegisterStudent() {
       
       </form>
       
+      </div>
       </div>
   )
 }
